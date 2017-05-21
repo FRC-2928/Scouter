1eca7cfdfed611f2dab7cc3661e3cba98fcdc4f4
robotbailee <bailee.barrick@gmail.com>
2017-05-20 17:45:48 -0700

    added stuff

--- src/cljs/scouter/main.cljs
+++ src/cljs/scouter/main.cljs
@@ -1,9 +1,31 @@
 (ns scouter.main
   (:require [reagent.core :as r]))
-
+  
+(def state (r/atom {:score 0 :team "xxxx" }))
+  
 (defn test-component [message]
   [:h1 message])
+ 
+(defn button [value change key]
+	[:button {:on-click #(swap! value (fn [v] (assoc-in v [key] (+ (js/Number change) (js/Number (:score v))))))}
+	change])
+	
+(defn number-input [value key]
+  [:input {:type "number"
+           :value (key @value)
+           :on-change #(reset! value (assoc-in @value [key] (-> % .-target .-value)))}])
 
+(defn score-component [value before after key]
+	[:div.numberinput
+		(map #(button value % key) before)
+		[number-input value key]
+		(map #(button value % key) after)])
+	
+
+		   
 (defn ^:export run []
-  (r/render [test-component "hello"] 
+  (r/render [score-component state [] [1 5] :score]
             (.-body js/document)))
+
+			
+			
